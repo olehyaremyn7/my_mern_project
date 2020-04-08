953a87ff291a30747fa3cf9340889234a53b5c33
Oleh Yaremyn <oleh_yaremyn@ukr.net>
2020-04-08 23:35:56 +0300

    React frontend done, Authorization done, write to the MongoDB done Links are working, HTTP req are working

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const mongoose = require('mongoose');
 const app = express();
 const PORT = config.get('port') || 5000;
 
+app.use(express.json({ extended: true }));
 app.use('/api/auth', require('./routes/auth.routes'));
+app.use('/api/link', require('./routes/link.routes'));
 
 async function start() {
     try {
