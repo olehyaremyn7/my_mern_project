7a2b3bb47d81b18c38dd70d122144e5b6e03cc3f
Oleh Yaremyn <oleh_yaremyn@ukr.net>
2020-04-08 17:54:17 +0300

    Before installation bcryptjs

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,8 +1,22 @@
 const {Router} = require('express');
+const User = require('../models/User');
 const router = Router();
 
 // /api/auth/register
 router.post('/register', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const candidate = await User.findOne({ email });
+
+        if (candidate) {
+            return  res.status(400).json({ message: 'Такий користувач уже існує' })
+        }
+
+
+
+    } catch (e) {
+
+    }
 
 });
 
