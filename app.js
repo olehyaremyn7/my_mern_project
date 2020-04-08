85dfde4bc5bc9610bd6a0c3bbb7f0b91d72dfe02
Oleh Yaremyn <oleh_yaremyn@ukr.net>
2020-04-08 17:31:12 +0300

    Server build and Mongo connect

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const config = require('config');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = config.get('port') || 5000;
+
+app.use('/api/auth', require('./routes/auth.routes'));
+
+async function start() {
+    try {
+        await mongoose.connect(config.get('mongoUri'), {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        });
+
+        app.listen(PORT, () => {
+            console.log(`App has been started on port ${PORT}...`)
+        });
+
+    } catch (e) {
+        console.log('Server error', e.message);
+        process.exit(1)
+    }
+}
+
+start();
+
